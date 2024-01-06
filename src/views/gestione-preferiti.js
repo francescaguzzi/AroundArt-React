d6d96cb3e90c72aee041c93009ee5b4ae9caa13a
giordu <giordana.martucci@studio.unibo.it>
2024-01-06 18:55:23 +0100

    fix gestionepreferiti

--- src/views/gestione-preferiti.js
+++ src/views/gestione-preferiti.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import { Helmet } from 'react-helmet'
 
@@ -7,6 +7,16 @@ import MainMenu from '../components/main-menu'
 import './gestione-preferiti.css'
 
 const GestionePreferiti = (props) => {
+  const listFavorites = [
+    {artista: "Exit Enter", image_src: "/opere/exit_via-belle-arti-%232_0-200h.jpg", indirizzo: "Via delle Belle Arti"},
+    {artista: "EricaIlCane", image_src: "/opere/serranda_via%20mascarella%2026a%2C%20modo%20-200h.jpg", indirizzo: "via Mascarella 26A, libreria Moda Infoshop"},
+    {artista: "About Ponny", image_src: "/opere/via-del-pratello-89_2b_carlos-atoche-200h.jpg", indirizzo: "Via del Pratello, 90"},
+    {titolo: "500 Anni dalla Conquista dell'America", artista: "Luis Gutierrez", image_src: "/opere/gutierrez_zamboni3-200h.png", indirizzo: "Via Zamboni 38"},
+    {artista: "Arianna Vauro", image_src: "/opere/vicolo%20facchini_ariannavauro_0-200h.jpg", indirizzo: "Vicolo de Facchini 7/2"}
+  ]
+
+  const [list, setList] = useState(listFavorites);
+
   return (
     <div className="gestione-preferiti-container">
       <Helmet>
@@ -17,34 +27,18 @@ const GestionePreferiti = (props) => {
         />
       </Helmet>
       <div className="gestione-preferiti-lista-opere">
-        <OperaLista
-          artista="Exit Enter"
-          image_src="/opere/exit_via-belle-arti-%232_0-200h.jpg"
-          indirizzo="Via delle Belle Arti"
-          rootClassName="opera-lista-root-class-name"
-        ></OperaLista>
-        <OperaLista
-          artista="EricaIlCane"
-          image_src="/opere/serranda_via%20mascarella%2026a%2C%20modo%20-200h.jpg"
-          indirizzo="via Mascarella 26A, libreria Moda Infoshop"
-        ></OperaLista>
-        <OperaLista
-          artista="About Ponny"
-          image_src="/opere/via-del-pratello-89_2b_carlos-atoche-200h.jpg"
-          indirizzo="Via del Pratello, 90"
-        ></OperaLista>
-        <OperaLista
-          titolo="500 Anni dalla Conquista dell'America"
-          artista="Luis Gutierrez"
-          image_src="/opere/gutierrez_zamboni3-200h.png"
-          indirizzo="Via Zamboni 38"
-        ></OperaLista>
-        <OperaLista
-          artista="Arianna Vauro"
-          image_src="/opere/vicolo%20facchini_ariannavauro_0-200h.jpg"
-          indirizzo="Vicolo de Facchini 7/2"
-          rootClassName="opera-lista-root-class-name1"
-        ></OperaLista>
+          {list.map((item, index) => (
+            <OperaLista
+              key={index}
+              index={index}
+              artista={item.artista}
+              image_src={item.image_src}
+              indirizzo={item.indirizzo}
+              titolo={item.titolo}
+              list={list}
+              setList={setList}
+            ></OperaLista>
+          ))}
       </div>
       <button type="button" className="gestione-preferiti-button button">
         Crea il tuo itinerario!
